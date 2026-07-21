629d926d88996153a08d650ee503744329e84165
ShravanK5 <shravankadam2444@gmail.com>
2026-07-21 20:53:04 +0530

    Time: 0 ms (100%), Space: 22.7 MB (78.15%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -11,41 +11,48 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
+        //step1 calculate mid using slow and fast 
+        ListNode * slow = head;
+        ListNode * fast = head;
 
-        ListNode* slow = head;
-        ListNode* fast = head;
-
-        while (fast != NULL && fast->next != NULL) {
+        while(fast != NULL && fast->next != NULL)
+        {
             fast = fast->next->next;
             slow = slow->next;
         }
-        ListNode* second = slow->next;
+        //creating second list ie splitting the list into half 
+        ListNode * second = slow->next;
         slow->next = NULL;
 
-        ListNode* curr = second;
-        ListNode* prev = NULL;
-        ListNode* next = NULL;
+        //step2 mid se aage wali ll ko reverse karo
+        ListNode * curr = second;
+        ListNode * prev = NULL;
+        ListNode * next = NULL;        
 
-        while (curr != NULL) {
+        while(curr != NULL)
+        {
             next = curr->next;
             curr->next = prev;
             prev = curr;
-            curr = next;
+            curr= next;
         }
+            second = prev; // second revered ll
+            ListNode * first = head ; // first ll until mid
 
-        ListNode* first = head;
-        second = prev;
 
-        while (second != NULL) {
+        // merge in such a way ek elemnet first element ka 2nd element dusre ll ka 
 
-            ListNode* temp1 = first->next;
-            ListNode* temp2 = second->next;
+        while(second != NULL)
+        {
+            ListNode * temp1 = first->next;
+            ListNode * temp2 = second -> next;
 
-            first->next = second;
-            second->next = temp1;
+            first -> next = second;
+            second -> next = temp1;
 
             first = temp1;
             second = temp2;
         }
+
     }
 };
