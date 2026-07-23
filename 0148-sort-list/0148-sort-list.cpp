a814ddea01030faedb40bd95e92ee7183b13f5e1
ShravanK5 <shravankadam2444@gmail.com>
2026-07-23 17:20:55 +0530

    Time: 50 ms (28.88%), Space: 75.8 MB (20.81%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,91 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+ListNode * findmid( ListNode * head)
+{
+    ListNode * slow = head;
+    ListNode * fast = head->next;
+
+    while(fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+ListNode * merge (ListNode * left , ListNode * right)
+{
+    if(left == NULL)
+    return right;
+
+    if(right == NULL)
+    return left;
+
+    ListNode * ans= new ListNode(-1);
+    ListNode * temp = ans;
+
+    while(left != NULL && right != NULL)
+    {
+        if( left->val < right->val)
+        {
+            temp ->next =left;
+            temp = left;
+            left = left->next;
+        }
+        else
+        {
+            temp ->next = right;
+            temp = right;
+            right = right->next;
+        }
+    }
+    while(left != NULL)
+    {
+        temp ->next =left;
+            temp = left;
+            left = left->next;
+    }
+    while(right != NULL)
+    {
+        temp ->next = right;
+            temp = right;
+        right = right->next;
+    }
+    ans = ans->next;
+    return ans;
+
+}
+
+
+public:
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+        return head;
+    // calculate mid and break into two 
+    ListNode * mid = findmid(head);
+
+    ListNode* left = head;
+    ListNode * right = mid->next;
+    mid->next = NULL;
+
+    //recursive call to brek 
+    left = sortList(left);
+    right = sortList(right);
+
+
+    //merge two lls
+    ListNode * result = merge(left, right);
+    return result;
+
+    }
+};
