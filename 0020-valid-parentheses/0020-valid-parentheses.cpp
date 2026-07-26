0df84487fbb41f9fce7bfd62caca359f17fd187d
ShravanK5 <shravankadam2444@gmail.com>
2026-07-26 13:34:34 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.47%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+
+        for(char ch : s)
+        {
+            if(ch == '(' || ch == '{' || ch== '[')
+            {
+                st.push(ch);
+            }
+            else
+            {
+                if(st.empty())
+                {
+                    return false;
+                }
+                if(ch == ')' && st.top() != '(')
+                return false;
+                else if(ch == '}' && st.top() != '{')
+                return false;
+                else if( ch== ']' && st.top()!= '[')
+                return false;
+                
+                else{
+                    st.pop();
+                }
+            }
+        }
+        if(st.empty())
+        return true;
+        
+        else
+        return false;
+    }
+};
